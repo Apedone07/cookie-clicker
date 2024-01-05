a71f67cadaf62d6c2d3fc97a0f1afcbef368d324
Apedone07 <105564609+Apedone07@users.noreply.github.com>
2024-01-05 16:10:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-let button = document.querySelector('button')
+let button = document.querySelector('button');
 
 let score = 0;
-button.addEventListener("click", increaseScore)
+
+button.addEventListener("click", increaseScore);
 
 function increaseScore() {
     score++;
+    document.querySelector('#score').innerHTML = 'Score: ' + score;
     console.log('Score: '  + score);
-    
-}
-document.querySelector('#score').innerHTML = 'Score: ' + score;
